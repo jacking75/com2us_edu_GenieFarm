83e1735bd39aeb6ea4f449113788e4b10310b87f
Dosoon <dosoon.in.cp@gmail.com>
2023-07-18 17:32:31 +0900

    [Feat] EventId 생성 클래스 구현

---
+++ GenieFarm/EventIdGenerator.cs
@@ -0,0 +1,12 @@
+﻿public static class EventIdGenerator
+{
+    public static EventId Create(ErrorCode errorCode)
+    {
+        return new EventId((UInt16)errorCode, errorCode.ToString());
+    }
+
+    public static EventId Create(UInt16 eventId, string eventIdName)
+    {
+        return new EventId(eventId, eventIdName);
+    }
+}
