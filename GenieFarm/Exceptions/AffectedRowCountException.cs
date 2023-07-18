b78131f67042fe78e96d7accfa011a839d831985
Dosoon <dosoon.in.cp@gmail.com>
2023-07-18 11:48:19 +0900

    [Feat] 최초 접속시 기본 아이템 생성에 대한 Exception 추가

---
+++ GenieFarm/Exceptions/AffectedRowCountException.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+public class AffectedRowCountOutOfRangeException : Exception
+{
+    public Int32 _affectedRowCount { get; set; }
+    public Int32 _defaultItemCount { get; set; }
+
+    public AffectedRowCountOutOfRangeException()
+    {
+    }
+
+    public AffectedRowCountOutOfRangeException(string? message) : base(message)
+    {
+    }
+
+    public AffectedRowCountOutOfRangeException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected AffectedRowCountOutOfRangeException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public AffectedRowCountOutOfRangeException(Int32 affectedRowCount, Int32 defaultItemCount)
+    {
+        _affectedRowCount = affectedRowCount;
+        _defaultItemCount = defaultItemCount;
+    }
+}
